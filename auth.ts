488b0921bed8897a5752e2c28a6f5d03f36c95ac
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-10-02 19:40:14 -0400

    Apply Auth.js split auth edge runtime strat

--- auth.ts
+++ auth.ts
@@ -1,6 +1,11 @@
 import NextAuth from "next-auth";
-import Google from "next-auth/providers/google";
+import authConfig from "./auth.config";
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google],
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "./db";
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  session: { strategy: "jwt" },
+  ...authConfig,
 });
