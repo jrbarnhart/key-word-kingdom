1bdb45624a598a0cc4ce62865d9100ab619df15a
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-10-02 21:27:12 -0400

    Add link to game route

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,23 @@
+import Link from "next/link";
+
 export default async function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-b from-blue-600 to-purple-800">
-      {/* Logo / Title */}
-      <h1 className="text-5xl font-bold text-white mb-8">Key Word Kingdom</h1>
+    <div className="flex flex-col items-center min-h-screen bg-gradient-to-b from-blue-600 to-purple-800">
+      <h1 className="mt-20 text-5xl font-bold text-white mb-8">
+        Key Word Kingdom
+      </h1>
 
-      {/* Tagline */}
       <p className="text-lg text-gray-200 mb-12">
         Enter the kingdom and master the art of words!
       </p>
 
-      {/* Footer */}
+      <Link
+        href={"/game"}
+        className="px-6 py-3 bg-yellow-500 hover:bg-yellow-600 text-black font-semibold text-base rounded-md shadow-lg"
+      >
+        Play
+      </Link>
+
       <footer className="absolute bottom-4 text-gray-300">
         <a href={"https://www.joshuarbarnhart.com"}>My Portfolio</a>
       </footer>
