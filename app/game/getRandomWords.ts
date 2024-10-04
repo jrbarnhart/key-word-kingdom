3825d83c7c3cc72374574b3e90c50889cc12282d
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-10-04 18:00:07 -0400

    Remove old hook and create server side fs function

---
+++ app/game/getRandomWords.ts
@@ -0,0 +1,23 @@
+import { promises as fs } from "fs";
+
+export default async function getRandomWords(count: number = 1) {
+  const file = await fs.readFile(
+    process.cwd() + "/app/_lib/2of12inf.txt",
+    "utf8"
+  );
+
+  const splitFile = file
+    .split("\n")
+    .map((line) => {
+      return line.trim();
+    })
+    .filter((line) => !line.includes("%"));
+
+  const randomWords: string[] = [];
+
+  for (let i = 0; i < count; i++) {
+    randomWords.push(splitFile[Math.floor(Math.random() * splitFile.length)]);
+  }
+
+  console.log(randomWords);
+}
