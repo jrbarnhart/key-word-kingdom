2a01adfd932f5e4a07e535a61e2b3f43bf15e39a
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-10-04 19:11:58 -0400

    Add fn for getting random char set based on key word

---
+++ app/game/getMoreChars.ts
@@ -0,0 +1,31 @@
+export function getMoreChars(keyWord: string, count: number = 9) {
+  const alphaArray = "abcdefghijklmnopqrstuvwxyz".split("");
+  const alphaSet = new Set(alphaArray);
+
+  const targetCount = count > 0 && count <= 26 ? Math.floor(count) : 9;
+
+  const strArray = keyWord.split("");
+
+  const uniqueCharSet = new Set<string>();
+
+  for (const char of strArray) {
+    uniqueCharSet.add(char);
+  }
+
+  for (const char of Array.from(uniqueCharSet)) {
+    alphaSet.delete(char);
+  }
+
+  for (let i = uniqueCharSet.size; i < targetCount; i++) {
+    const randomIndex = Math.floor(Math.random() * alphaSet.size);
+    const randomChar = Array.from(alphaSet)[randomIndex];
+    uniqueCharSet.add(randomChar);
+    alphaSet.delete(randomChar);
+  }
+
+  const chars = Array.from(uniqueCharSet);
+
+  console.log(chars);
+
+  return chars;
+}
