7da5d91d3524d203dda1fc0e5d5ca6fd8bff4dab
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-10-10 17:49:09 -0400

    Expand options for getWords and make results more robust

--- app/game/getWords.ts
+++ app/game/getWords.ts
@@ -1,6 +1,14 @@
 import { promises as fs } from "fs";
 
-export default async function getWords(count: number = 1) {
+export default async function getWords({
+  ...options
+}: {
+  totalKeyWords: number;
+  maxLength: number;
+}) {
+  const MAX_KEY_WORDS = 100;
+  const { totalKeyWords, maxLength } = options;
+
   const file = await fs.readFile(
     process.cwd() + "/app/_lib/2of12inf.txt",
     "utf8"
@@ -11,12 +19,28 @@ export default async function getWords(count: number = 1) {
     .map((line) => {
       return line.trim();
     })
-    .filter((line) => !line.includes("%"));
+    .filter((line) => !line.includes("%") && line.length <= maxLength);
 
-  const randomWords: string[] = [];
+  // Shuffle the splitFile array
+  for (let i = splitFile.length - 1; i >= 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [splitFile[i], splitFile[j]] = [splitFile[j], splitFile[i]];
+  }
 
-  for (let i = 0; i < count; i++) {
-    randomWords.push(splitFile[Math.floor(Math.random() * splitFile.length)]);
+  let randomWords = splitFile.slice(
+    0,
+    totalKeyWords >= 1 && totalKeyWords <= MAX_KEY_WORDS ? totalKeyWords : 1
+  );
+
+  // If there aren't enough random words then just add the word missing until there are enough
+  if (
+    totalKeyWords > 0 &&
+    totalKeyWords <= MAX_KEY_WORDS &&
+    randomWords.length < totalKeyWords
+  ) {
+    randomWords = Array(totalKeyWords).fill("missing");
+  } else if (randomWords.length !== totalKeyWords) {
+    randomWords = ["missing"];
   }
 
   const words = { wordArray: splitFile, keyWords: randomWords };
