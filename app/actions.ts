78c135e42e7e165c3386089bf41c7568647db678
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-10-10 17:57:04 -0400

    Add server action for getting words

---
+++ app/actions.ts
@@ -0,0 +1,16 @@
+"use server";
+
+import getWords from "./game/getWords";
+
+export async function getWordsAction({
+  ...options
+}: {
+  totalKeyWords: number;
+  maxLength: number;
+}) {
+  const { totalKeyWords, maxLength } = options;
+  // Get the words
+  const words = await getWords({ totalKeyWords, maxLength });
+  // Return the words and relevant options
+  return words;
+}
