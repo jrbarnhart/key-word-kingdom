629be38f5540c12fabeca9ec6c5f8c8abf90f609
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-10-11 16:21:13 -0400

    Add quit button to nav bar and remove logout

--- app/_components/nav/NavBar.tsx
+++ app/_components/nav/NavBar.tsx
@@ -1,20 +1,28 @@
 import { auth } from "@/auth";
-import LogoutButton from "../auth/LogoutButton";
 import LoginButton from "../auth/LoginButton";
+import Link from "next/link";
 
 export default async function NavBar() {
   const session = await auth();
 
   return (
     <div className="absolute top-0 w-full flex justify-center">
-      <nav className="absolute top-0 w-full p-1 max-w-2xl flex gap-3 items-center justify-end">
+      <nav className="absolute top-0 w-full p-1 max-w-2xl flex gap-3 items-center justify-between">
         {session?.user ? (
           <>
-            <p>Hello, {session.user.name}</p>
-            <LogoutButton />
+            <p>Player: {session.user.name}</p>
+            <Link
+              href={process.env.ROOT_ROUTE ?? "/"}
+              className="bg-red-400 text-black font-bold rounded-md border border-black p-1"
+            >
+              Quit Game
+            </Link>
           </>
         ) : (
-          <LoginButton />
+          <>
+            <p>Player: User</p>
+            <LoginButton />
+          </>
         )}
       </nav>
     </div>
