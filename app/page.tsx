179c54e1425165dc82adc36e0a3e635ac472cf5c
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-10-13 17:32:38 -0400

    Remove footer and add game menu

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,17 @@
-import Link from "next/link";
+import GameMenu from "./_components/ui/GameMenu";
 
 export default async function Home() {
   return (
     <div className="flex flex-col items-center min-h-screen p-2">
-      <h1 className="mt-20 text-5xl font-bold text-white mb-8">
+      <h1 className="mt-5 text-5xl font-bold text-white mb-8">
         Key Word Kingdom
       </h1>
 
-      <p className="text-lg text-gray-200 mb-12">
+      <p className="text-lg text-gray-200 mb-10">
         Enter the kingdom and master the art of words!
       </p>
 
-      <Link
-        href={"/game"}
-        className="px-6 py-3 bg-yellow-500 hover:bg-yellow-600 text-black font-semibold text-base rounded-md shadow-lg"
-      >
-        Play
-      </Link>
-
-      <footer className="absolute bottom-4 text-gray-300">
-        <a href={"https://www.joshuarbarnhart.com"}>My Portfolio</a>
-      </footer>
+      <GameMenu />
     </div>
   );
 }
