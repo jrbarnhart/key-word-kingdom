a2d194a4ce9cba5db92a17d94e2d2128170c6d51
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-10-13 18:32:40 -0400

    Add controlled input for custom total key words

--- app/_components/ui/GameMenu.tsx
+++ app/_components/ui/GameMenu.tsx
@@ -11,6 +11,29 @@ export default function GameMenu() {
   const [timer, setTimer] = useState(TIMER_DEFAULT);
   const [totalKeyWords, setTotalKeyWords] = useState(TOTAL_DEFAULT);
 
+  const [totalInputValue, setTotalInputValue] = useState<string>("");
+
+  const handleTotalInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    // Clamp the input value
+    const value = !isNaN(parseInt(event.target.value))
+      ? parseInt(event.target.value)
+      : 0;
+
+    if (value < 0) {
+      setTotalInputValue("1");
+      setTotalKeyWords(1);
+    } else if (value > 100) {
+      setTotalInputValue("100");
+      setTotalKeyWords(100);
+    } else if (value === 0) {
+      setTotalInputValue("");
+      setTotalKeyWords(1);
+    } else {
+      setTotalInputValue(value.toString());
+      setTotalKeyWords(value);
+    }
+  };
+
   return (
     <div className="w-full max-w-lg border-4 border-neutral-700 bg-gradient-to-t from-blue-500 to-purple-400 rounded-md flex flex-col items-center gap-3">
       <button className="bg-yellow-500 hover:bg-yellow-300 border-2 border-neutral-700 rounded-md p-3 font-bold text-lg mt-3">
@@ -21,14 +44,14 @@ export default function GameMenu() {
         {/* Length Options */}
         <div className="word length options container">
           <h2>Word Length</h2>
-          <div className="flex gap-1 justify-between items-center bg-purple-300 border h-12 border-blue-800 rounded-md p-1">
-            <button className="bg-sky-500 border hover:bg-sky-300 border-blue-500 h-full w-full rounded-md">
+          <div className="flex gap-1 justify-between items-center bg-purple-300 border-2 h-12 border-blue-800 rounded-md p-1">
+            <button className="bg-sky-500 border hover:bg-sky-300 border-blue-500 h-full w-full px-2 rounded-md">
               Small
             </button>
-            <button className="bg-sky-500 border hover:bg-sky-300 border-blue-500 h-full w-full rounded-md">
+            <button className="bg-sky-500 border hover:bg-sky-300 border-blue-500 h-full w-full px-2 rounded-md">
               Med
             </button>
-            <button className="bg-sky-500 border hover:bg-sky-300 border-blue-500 h-full w-full rounded-md">
+            <button className="bg-sky-500 border hover:bg-sky-300 border-blue-500 h-full w-full px-2 rounded-md">
               Long
             </button>
           </div>
@@ -36,41 +59,37 @@ export default function GameMenu() {
         {/* Timer Options */}
         <div className="timer options container">
           <h2>Timer</h2>
-          <div className="flex gap-1 justify-between items-center bg-purple-300 border h-12 border-blue-800 rounded-md p-1">
-            <button className="bg-sky-500 border hover:bg-sky-300 border-blue-500 h-full w-full rounded-md">
+          <div className="flex gap-1 justify-between items-center bg-purple-300 border-2 h-12 border-blue-800 rounded-md p-1">
+            <button className="bg-sky-500 border hover:bg-sky-300 border-blue-500 h-full w-full px-2 rounded-md">
               None
             </button>
-            <button className="bg-sky-500 border hover:bg-sky-300 border-blue-500 h-full w-full rounded-md">
+            <button className="bg-sky-500 border hover:bg-sky-300 border-blue-500 h-full w-full px-2 rounded-md">
               Normal
             </button>
-            <h3>Time Attack:</h3>
-            <button className="bg-sky-500 border hover:bg-sky-300 border-blue-500 h-full w-full rounded-md">
-              Easy
-            </button>
-            <button className="bg-sky-500 border hover:bg-sky-300 border-blue-500 h-full w-full rounded-md">
-              Med
-            </button>
-            <button className="bg-sky-500 border hover:bg-sky-300 border-blue-500 h-full w-full rounded-md">
-              Hard
+            <button className="bg-sky-500 border hover:bg-sky-300 border-blue-500 h-full w-full px-2 rounded-md">
+              Time Attack
             </button>
           </div>
         </div>
         {/* Total Key Word Options */}
         <div className="total keys options container">
           <h2>Total Key Words</h2>
-          <div className="flex gap-1 justify-between items-center bg-purple-300 border h-12 border-blue-800 rounded-md p-1">
-            <button className="bg-sky-500 border hover:bg-sky-300 border-blue-500 h-full w-full rounded-md">
+          <div className="flex gap-1 justify-between items-center bg-purple-300 border-2 h-12 border-blue-800 rounded-md p-1">
+            <button className="bg-sky-500 border hover:bg-sky-300 border-blue-500 h-full w-full px-2 rounded-md">
               1
             </button>
-            <button className="bg-sky-500 border hover:bg-sky-300 border-blue-500 h-full w-full rounded-md">
+            <button className="bg-sky-500 border hover:bg-sky-300 border-blue-500 h-full w-full px-2 rounded-md">
               3
             </button>
-            <button className="bg-sky-500 border hover:bg-sky-300 border-blue-500 h-full w-full rounded-md">
+            <button className="bg-sky-500 border hover:bg-sky-300 border-blue-500 h-full w-full px-2 rounded-md">
               6
             </button>
-            <button className="bg-sky-500 border hover:bg-sky-300 border-blue-500 h-full w-full rounded-md">
-              X
-            </button>
+            <input
+              className="bg-sky-500 border hover:bg-sky-300 border-blue-500 h-full w-full px-2 rounded-md placeholder:text-black text-center"
+              placeholder="Custom"
+              value={totalInputValue}
+              onChange={handleTotalInput}
+            />
           </div>
         </div>
       </div>
